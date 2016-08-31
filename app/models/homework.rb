2d029e4f8b5d337c1a750478b2ba15dea2795c39
Ameen Demidem <ameen.demidem@gmail.com>
2016-08-31 14:27:25 -0400

    Migrate models and seeds

---
+++ app/models/homework.rb
@@ -0,0 +1,7 @@
+class Homework < ActiveRecord::Base
+
+  belongs_to :user
+  
+  has_many :tasks
+  has_many :comments
+end
---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ActiveRecord::Base
+
+  has_secure_password
+
+  belongs_to :teacher, class_name: "User", optional: true
+
+  has_many :students, class_name: "User", foreign_key: "teacher_id"
+  has_many :homeworks
+  has_many :comments, through: :homeworks
+  has_many :tasks, through: :homeworks
+
+  validates :name, presence: true
+  validates :username, presence: true
+end
