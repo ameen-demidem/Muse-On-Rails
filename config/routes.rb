c54cc9bc309f5627288501f67999eddf0a2d2f8f
Ameen Demidem <ameen.demidem@gmail.com>
2016-09-02 09:38:43 -0400

    Add the welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
  
-  root "sessions#new"
+  root "welcome#index"
+
   resource :session, only: [:new, :create, :destroy]
   resource :user, only: [:new, :create, :destroy]
 
