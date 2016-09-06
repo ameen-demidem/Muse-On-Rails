4cf3d79f27742a03de52ba3f1253bbfe9b9f1bea
Ameen Demidem <ameen.demidem@gmail.com>
2016-09-06 17:43:01 -0400

    Add basic show page for the parent's view

--- app/controllers/parent/children_controller.rb
+++ app/controllers/parent/children_controller.rb
@@ -1,5 +1,7 @@
 class Parent::ChildrenController < ApplicationController
   before_action :check_authentication
+  before_action :check_authorization
+  before_action :load_child, only: [:show]
 
   def index
     @parent = current_user
@@ -7,5 +9,16 @@ class Parent::ChildrenController < ApplicationController
   end
 
   def show
+    @parent = current_user
+  end
+
+  protected
+
+  def check_authorization
+  end
+
+  def load_child
+    @child = User.find_by(id: params[:id])
+    redirect_to parent_children_path unless @child.parent == current_user
   end
 end
