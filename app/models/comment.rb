69b2067099371af5ea004f43f4ead6b39c6e62b0
Ameen Demidem <ameen.demidem@gmail.com>
2016-09-13 18:02:37 -0400

    Enable both teacher and student to attach files to their comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,8 @@ class Comment < ActiveRecord::Base
   belongs_to :task
   belongs_to :user
 
+  validates :feedback, presence: true
+
   has_attached_file :attachment
   validates_attachment_content_type :attachment,
     content_type: /.*/,
